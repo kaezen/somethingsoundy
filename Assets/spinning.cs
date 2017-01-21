fdcaadfe87042cf577859d1162ad82ec2ae2eb92
kaezen <haneryu@gmail.com>
2017-01-20 20:34:48 -0500

    something please work
    player movement

---
+++ Assets/spinning.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spinning : MonoBehaviour {
+
+    float h = 0;
+    public float rotateSpeed = 1;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+            h = Input.GetAxis("Horizontal");
+        
+        //print(h);
+
+        h *= .9f;
+        if (h < .3f && h > -.3f) h = 0;
+
+        this.transform.Rotate(0, h*rotateSpeed, 0);
+	}
+}
