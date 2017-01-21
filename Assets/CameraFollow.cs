0b49bc4264244188e8dc7848f3920ce88adc031a
ScottTheIdeaGuy <gigaroidbiz@gmail.com>
2017-01-21 07:28:46 -0500

    added player movement

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour 
+{
+	public Transform myPlayah;
+	
+	void Start () 
+	{
+		
+	}
+	
+	void Update () 
+	{
+		Vector3 pos = transform.position;
+		pos.x = myPlayah.position.x;
+		pos.y = myPlayah.position.y + 5;
+		pos.z = myPlayah.position.z - 2;
+		
+		transform.position = pos;
+	}
+}
